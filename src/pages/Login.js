c45f2fef80ed0ccb80512a1ae7670787bad6f5aa
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-06-22 20:48:09 -0300

    fazendo req 1 - comitando para atualizar a branch

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,76 @@
 import React from 'react';
 
+const MIN_LENGTH_PASSWORD = 6;
+const EMAIL_REGEX = /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/;
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emailInputValue: '',
+      passwordInputValue: '',
+      emailIsValid: false,
+      passwordIsValid: false,
+    };
+    this.onChangeInputValue = this.onChangeInputValue.bind(this);
+  }
+
+  onChangeInputValue({ target }) {
+    this.setState({
+      [target.name]: target.value,
+    }, this.validateInput);
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+  }
+
+  validateInput() {
+    const { emailInputValue, passwordInputValue } = this.state;
+
+    if (passwordInputValue.length >= MIN_LENGTH_PASSWORD) {
+      this.setState({ passwordIsValid: true });
+    } else {
+      this.setState({ passwordIsValid: false });
+    }
+    if (EMAIL_REGEX.test(emailInputValue)) {
+      this.setState({ emailIsValid: true });
+    } else {
+      this.setState({ passwordIsValid: false });
+    }
+  }
+
   render() {
-    return <div>Login</div>;
+    const {
+      emailInputValue,
+      passwordInputValue,
+      emailIsValid,
+      passwordIsValid,
+    } = this.state;
+
+    return (
+      <form onSubmit={ this.onSubmit }>
+        <input
+          data-testid="email-input"
+          name="emailInputValue"
+          type="email"
+          onChange={ this.onChangeInputValue }
+          value={ emailInputValue }
+        />
+        <input
+          data-testid="email-input"
+          name="passwordInputValue"
+          type="password"
+          onChange={ this.onChangeInputValue }
+          value={ passwordInputValue }
+        />
+        <button
+          type="submit"
+          disabled={ !(emailIsValid && passwordIsValid) }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
